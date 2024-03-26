029ea32168b73186a844c440c74abee0a5a81fc0
Odia Adodo <subzbelow@gmail.com>
2024-03-27 00:00:15 +0100

    Full project refactoring to provide better fil arrangement

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// environment.prod.ts (production)
+export const environment = {
+  production: true,
+  googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
+  mapBoxGlAccessToken:"token",
+  baseUri:"localhost://3000/"
+};
+
