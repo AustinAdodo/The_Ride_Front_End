a4c43e4d7a30deaf98dc2a0122e81fd0cddf4880
Odia Adodo <subzbelow@gmail.com>
2024-03-26 16:37:18 +0100

    Updated app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {provideRouter, RouterLink, RouterOutlet} from '@angular/router';
+import {Router} from '@angular/router';
+import {routes} from './app.routes';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Ride';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+  }
 }
