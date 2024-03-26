328d3e6d962cfbd338c682dcdaee0d6982f00b66
Odia Adodo <subzbelow@gmail.com>
2024-03-26 16:36:24 +0100

    Configured routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {HomeComponent} from "./home/home.component";
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  { path: '/login', component: LoginComponent },
+  { path: '/index', component: HomeComponent }
+];
