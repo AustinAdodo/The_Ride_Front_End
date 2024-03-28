24d62cf2017583391a07116836e8d7301afdce6a
Odia Adodo <subzbelow@gmail.com>
2024-03-28 17:59:47 +0100

    Update to AuthService.ts

---
+++ src/services/AuthService.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private loggedIn = new BehaviorSubject<boolean>(false);
+
+  get isLoggedIn() {
+    return this.loggedIn.asObservable(); //allow components to subscribe to the login state
+  }
+
+  constructor() { }
+
+  login() {
+    this.loggedIn.next(true);
+  }
+
+  logout() {
+    this.loggedIn.next(false);
+  }
+}
