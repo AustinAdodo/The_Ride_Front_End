f5132573a61b7160a341a3da93c61af8d8ba0ecb
Odia Adodo <subzbelow@gmail.com>
2024-03-28 18:00:28 +0100

    Updated app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import {Route} from '@angular/router';
 import {LoginComponent} from "../login/login.component";
 import {HomeComponent} from "../home/home.component";
 import {DriverLoginComponent} from '../driver-login/driver-login.component';
+import {MapComponent} from "../map/map.component";
+import {CreateDriverComponent} from "../create-driver/create-driver.component";
 
 
 export const routes: Route[] = [
   {path: 'login', component: LoginComponent},
   {path: 'index', component: HomeComponent},
-  {path: 'drive/login', component: DriverLoginComponent}
+  {path: 'drive/login', component: DriverLoginComponent},
+  {path: 'drive/create', component: CreateDriverComponent},
+  { path: 'usermap', component: MapComponent }
 ];
