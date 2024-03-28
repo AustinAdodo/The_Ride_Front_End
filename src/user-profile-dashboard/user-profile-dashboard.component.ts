c004142d37b646000687c636fb7600adb6561dca
Odia Adodo <subzbelow@gmail.com>
2024-03-28 17:59:19 +0100

    Added user-profile-dashboard.component.ts and its related members

---
+++ src/user-profile-dashboard/user-profile-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-profile-dashboard.component.html',
+  styleUrl: './user-profile-dashboard.component.css'
+})
+export class UserProfileDashboardComponent {
+
+}
