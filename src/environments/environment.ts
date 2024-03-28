3fc696311eb5d1a79f40f2b7e4cc4f049237aebe
Odia Adodo <subzbelow@gmail.com>
2024-03-28 18:01:50 +0100

    Update to create-driver.component.css

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
-  mapBoxGlAccessToken:"token",
-  baseUri:"localhost://3000/"
+  mapBoxGlAccessToken:"pk.eyJ1IjoiYXVzdGluYWRvZG8iLCJhIjoiY2x1YjR0YW5lMHM2ZDJqcWU1YXJ3MnlkMyJ9.BvGASOXt2kda28FUTlGNOg",
+  baseUri:"http://localhost:8080"
 };
-
+//sk.eyJ1IjoiYXVzdGluYWRvZG8iLCJhIjoiY2x1YjVxdXJmMHU1NzJrbzlqNTQ2ZTc2eiJ9.lTl6pfRaDUbaVEAFpJq17Q
