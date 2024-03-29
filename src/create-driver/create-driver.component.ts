86244b511f576fa46565e6af2a5b282351b62f9e
Odia Adodo <subzbelow@gmail.com>
2024-03-29 01:24:39 +0100

    Update

--- src/create-driver/create-driver.component.ts
+++ src/create-driver/create-driver.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { environment } from '../environments/environment';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-create-driver',
@@ -11,23 +11,25 @@ import { environment } from '../environments/environment';
   styleUrls: ['./create-driver.component.css']
 })
 export class CreateDriverComponent {
+  driver: string = "Driver";
   driverForm = new FormGroup({
     firstName: new FormControl('', Validators.required),
     middleName: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
     address: new FormControl('', Validators.required),
     carColor: new FormControl('', Validators.required),
-    carName: new FormControl('', Validators.required),
+    carBrand: new FormControl('', Validators.required),
   });
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   onSubmit() {
     const baseUrl = environment.baseUri;
-    const addDriverUrl = `${baseUrl}/addriver`;
+    const addPersonUrl = `${baseUrl}/signup`;
 
     if (this.driverForm.valid) {
-      this.http.post(addDriverUrl, this.driverForm.value).subscribe({
+      this.http.post(addPersonUrl, this.driverForm.value).subscribe({
         next: (response) => {
           console.log('Driver added successfully', response);
           this.driverForm.reset();
