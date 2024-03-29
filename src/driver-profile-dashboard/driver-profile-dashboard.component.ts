991ba1267087b42af84d47164b9811fdffe5744b
Odia Adodo <subzbelow@gmail.com>
2024-03-29 01:25:47 +0100

    Update

---
+++ src/driver-profile-dashboard/driver-profile-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-profile-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './driver-profile-dashboard.component.html',
+  styleUrl: './driver-profile-dashboard.component.css'
+})
+export class DriverProfileDashboardComponent {
+
+}
