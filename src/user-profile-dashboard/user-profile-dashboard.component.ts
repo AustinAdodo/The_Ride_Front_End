6614331cca642141b86242749142420b0533dddd
Odia Adodo <subzbelow@gmail.com>
2024-03-29 01:24:53 +0100

    Update

--- src/user-profile-dashboard/user-profile-dashboard.component.ts
+++ src/user-profile-dashboard/user-profile-dashboard.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CommonModule, NgOptimizedImage} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+interface RecentTrip {
+  destination: string;
+  date: string;
+  status: string;
+}
+
+interface FavoritePlace {
+  name: string;
+  location: string;
+}
 
 @Component({
   selector: 'app-user-profile-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgOptimizedImage,
+    CommonModule
+  ],
   templateUrl: './user-profile-dashboard.component.html',
-  styleUrl: './user-profile-dashboard.component.css'
+  styleUrls: ['./user-profile-dashboard.component.css']
 })
-export class UserProfileDashboardComponent {
+export class UserProfileDashboardComponent implements OnInit {
+  recentTrips: RecentTrip[] = [];
+  favoritePlaces: FavoritePlace[] = [];
+  showForm: boolean = false;
+  Rating : string = '/assets/5stars.png';
+  mastercard :string= '/assets/mastercard.png';
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loadRecentTrips();
+    this.loadFavoritePlaces();
+  }
+
+  loadRecentTrips(): void {
+    this.recentTrips = [
+      { destination: 'Park-Lane', date: '2024-03-22', status: 'Completed' },
+      { destination: 'Country Club', date: '2024-02-15', status: 'Completed' }
+    ];
+  }
+
+  loadFavoritePlaces(): void {
+    this.favoritePlaces = [
+      { name: 'Ikeja City Mall', location: 'Lagos' },
+      { name: 'Black bell Restaurant', location: 'Lagos' }
+      // Add more places here
+    ];
+  }
+  onSubmit() {
+    // Handle form submission here
+    console.log('Form submitted');
+    this.showForm = false;
+  }
 }
