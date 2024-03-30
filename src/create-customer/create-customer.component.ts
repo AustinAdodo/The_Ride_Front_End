bc10f9227263eaf3f351b727c54b6711bc308513
Odia Adodo <subzbelow@gmail.com>
2024-03-30 17:13:35 +0100

    Update to component

--- src/create-customer/create-customer.component.ts
+++ src/create-customer/create-customer.component.ts
@@ -1,37 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {FormGroup, FormControl, Validators, ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {environment} from '../environments/environment';
+import {FormExtension} from "../Extensions/FormExtension";
+import {NgIf} from "@angular/common";
 
 @Component({
-  selector: 'app-create-customer',
+  selector: 'create-user',
   standalone: true,
   imports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './create-customer.component.html',
   styleUrl: './create-customer.component.css'
 })
-export class CreateCustomerComponent {
-  customer: string = "Customer";
-  @Component({
-    selector: 'create-user',
-    standalone: true,
-    imports: [ReactiveFormsModule],
-    templateUrl: './create-driver.component.html',
-    styleUrls: ['./create-driver.component.css']
-  })
+
+export class CreateCustomerComponent implements OnInit{
+  hidePassword = true;
+
   UserForm = new FormGroup({
     firstName: new FormControl('', Validators.required),
-    middleName: new FormControl('', Validators.required),
+    sex: new FormControl(''),
+    middleName: new FormControl(''),
     lastName: new FormControl('', Validators.required),
     address: new FormControl('', Validators.required),
-    carColor: new FormControl('', Validators.required),
-    carName: new FormControl('', Validators.required),
-  });
+    email: new FormControl('', [Validators.required, Validators.email]),
+    Usertype: new FormControl('Customer'),
+    password: new FormControl('', Validators.required),
+    isOnlyMySexAllowed: new FormControl(''),
+    confirmPassword: new FormControl('', Validators.required)
+  }, {validators: FormExtension.confirmPasswordValidator('password', 'confirmPassword')});
+
 
   constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {}
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
   }
 
   onSubmit() {
@@ -43,8 +52,18 @@ export class CreateCustomerComponent {
           console.log('Driver added successfully', response);
           this.UserForm.reset();
         },
-        error: (error) => console.error('Error adding driver:', error)
+        error: (error) => console.error('Error adding user:', error)
       });
     }
   }
+
+  // dropdownClick() {
+  //   this.UserForm.get('isOnlyMySexAllowed')?.valueChanges.subscribe(value => {
+  //     const booleanValue = value === 'true';
+  //     // IMPORTANT: Directly updating the same control inside its valueChanges subscription
+  //     // can lead to infinite loops. To prevent this, use {emitEvent: false} option.
+  //     this.UserForm.get('isOnlyMySexAllowed')?.setValue(booleanValue, {emitEvent: booleanValue});
+  //     console.log("Dropdown changed to boolean:", booleanValue);
+  //   });
+  // }
 }
