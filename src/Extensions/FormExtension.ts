90ababaa54f78b329f2f0c0b279723cd6cbcee51
Odia Adodo <subzbelow@gmail.com>
2024-03-30 17:13:08 +0100

    Form Extension class handles confirm password validators

---
+++ src/Extensions/FormExtension.ts
@@ -0,0 +1,24 @@
+import {AbstractControl, FormGroup, ValidationErrors, ValidatorFn} from '@angular/forms';
+
+export class FormExtension {
+  static confirmPasswordValidator(controlName: string, matchingControlName: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const formGroup = control as FormGroup; // Cast the AbstractControl to FormGroup
+      const controlToCompare = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (!controlToCompare || !matchingControl) {
+        // Optionally, you could handle the error of controls not being found
+        return null; // or return an error object if necessary
+      }
+
+      if (controlToCompare.value !== matchingControl.value) {
+        matchingControl.setErrors({ notMatched: true });
+        return { notMatched: true }; // Return an error object
+      } else {
+        matchingControl.setErrors(null);
+        return null; // Indicate that there is no error
+      }
+    };
+  }
+}
