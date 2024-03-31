f367670074e79ebaf94fab22c70ceed86b2cc796
Odia Adodo <subzbelow@gmail.com>
2024-03-31 12:06:12 +0100

    Routing for Driver dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import {HomeComponent} from "../home/home.component";
 import {DriverLoginComponent} from '../driver-login/driver-login.component';
 import {MapComponent} from "../map/map.component";
 import {CreateDriverComponent} from "../create-driver/create-driver.component";
+import {CreateCustomerComponent} from "../create-customer/create-customer.component";
+import {DriverProfileDashboardComponent} from "../driver-profile-dashboard/driver-profile-dashboard.component";
 
 
 export const routes: Route[] = [
@@ -11,6 +13,8 @@ export const routes: Route[] = [
   {path: 'index', component: HomeComponent},
   {path: 'drive/login', component: DriverLoginComponent},
   {path: 'drive/create', component: CreateDriverComponent},
+  {path: 'drive/home', component: DriverProfileDashboardComponent},
+  {path: 'user/create', component: CreateCustomerComponent},
   {path: 'home', component: HomeComponent},
   { path: 'usermap', component: MapComponent }
 ];
