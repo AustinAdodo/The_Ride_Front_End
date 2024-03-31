656ec3a681e68173685a92209b8e17c8b76a9797
Odia Adodo <subzbelow@gmail.com>
2024-03-31 12:14:21 +0100

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   production: true,
   googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
   mapBoxGlAccessToken:"pk.eyJ1IjoiYXVzdGluYWRvZG8iLCJhIjoiY2x1YjR0YW5lMHM2ZDJqcWU1YXJ3MnlkMyJ9.BvGASOXt2kda28FUTlGNOg",
-  baseUri:"http://localhost:8080"
+  baseUri:"http://localhost:8080",
+  websocketurl:'http://localhost:8080/ws'
 };
 //sk.eyJ1IjoiYXVzdGluYWRvZG8iLCJhIjoiY2x1YjVxdXJmMHU1NzJrbzlqNTQ2ZTc2eiJ9.lTl6pfRaDUbaVEAFpJq17Q
