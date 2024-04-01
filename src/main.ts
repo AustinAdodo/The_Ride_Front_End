b67aebcd44c9410bdad221832c9ff37f4db2abdd
Odia Adodo <subzbelow@gmail.com>
2024-04-01 20:44:02 +0100

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+//(window as any).global = window; //polyfill work-around
+//import './global-shim';
+
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {AppComponent} from './app/app.component';
 import {environment} from "./environments/environment";
-import {enableProdMode} from "@angular/core";
+import {enableProdMode, importProvidersFrom} from "@angular/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
 
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom([BrowserAnimationsModule]),...appConfig.providers,]
+}).catch((err) => console.error(err));
