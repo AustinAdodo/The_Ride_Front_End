e1795cc93e17ccbcca8032343cc5607400da39de
Odia Adodo <subzbelow@gmail.com>
2024-04-01 15:51:38 +0100

    Update to map Component

--- src/map/map.component.ts
+++ src/map/map.component.ts
@@ -1,43 +1,34 @@
-import {Component, OnInit} from '@angular/core';
-import {GoogleMap, MapMarker} from "@angular/google-maps";
-import mapboxgl, {baseApiUrl} from 'mapbox-gl';
-import {NgForOf, NgIf} from "@angular/common";
-import { DriverService } from '../services/DriverService';
+import {Component, OnInit, AfterViewInit, Inject, PLATFORM_ID} from '@angular/core';
+//import {GoogleMap, MapMarker} from "@angular/google-maps";
+import mapboxgl from 'mapbox-gl';
+import {CommonModule} from "@angular/common";
+import {DriverService} from '../services/DriverService';
 import {environment} from '../environments/environment';
+import {isPlatformBrowser} from '@angular/common';
 
-const apiKey = environment.googleMapsApiKey;
 const map_Box_key = environment.mapBoxGlAccessToken;
+
 //User lat: 3.3581 and long: 6.6144
 
 @Component({
   selector: 'app-map',
   standalone: true,
   imports: [
-    MapMarker,
-    GoogleMap,
-    NgForOf,
-    NgIf
+    CommonModule
   ],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrls: ['./map.component.css']
 })
 
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
+  //Implement AfterViewInit
   isMapBoxMapToBeDisplayed: boolean = true;
-  isGoogleMapsToBeDisplayed: boolean = false;
-  mapOptions: google.maps.MapOptions = {
-    center: new google.maps.LatLng(3.3792, 6.5244), // default
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-  //mapbox
   map: mapboxgl.Map | undefined;
   styleURL = 'mapbox://styles/mapbox/streets-v11';
   accessToken = map_Box_key;
-  cars: { lat: number, lng: number, color: string }[] = [];
+  cars: Car[] = [];
 
-  constructor(private driverService: DriverService) {
+  constructor(private driverService: DriverService, @Inject(PLATFORM_ID) private platformId: Object) {
   }
 
   /**
@@ -46,43 +37,89 @@ export class MapComponent implements OnInit {
    * latitude and longitude are randomly changed
    *
    */
-
   ngOnInit(): void {
     this.getDrivers();
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: this.styleURL,
-      center: [3.3581, 6.6144], // Lagos coordinates [3.3792, 6.5244],
-      zoom: 13,
-      accessToken: this.accessToken
-    });
+  }
 
-    // Generate
-    for (let i = 0; i < 30; i++) {
-      const randomLat = 6.45 + Math.random() * 0.2;
-      const randomLng = 3.35 + Math.random() * 0.3;
-      const randomColor = this.getRandomColor();
-      this.cars.push({lat: randomLat, lng: randomLng, color: randomColor});
+  ngAfterViewInit(): void {
+    //this.initializeMap();
+    if (isPlatformBrowser(this.platformId)) {
+      this.initializeMap();
     }
+  }
 
-    //add real drivers
-    const uri: string = `${baseApiUrl}/drivers`;
+  /**
+   * map initialization
+   */
+  private initializeMap(): void {
+    if (!this.map) {
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        style: this.styleURL,
+        center: [3.3581, 6.6144],
+        zoom: 16,
+        accessToken: this.accessToken
+      });
+      this.map.on('load', () => {
+        this.addCarsToMap();
+        this.simulateCarMovement();
+      });
+    }
+  }
 
+  private createCarMarker(car: Car): mapboxgl.Marker {
+    if (isPlatformBrowser(this.platformId)) {
+      const el = document.createElement('div');
+      el.innerHTML = `
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="fill: ${car.color};">
+        <rect x="20" y="40" width="60" height="20" rx="5"/>
+        <circle cx="30" cy="70" r="10"/>
+        <circle cx="70" cy="70" r="10"/>
+        <circle cx="30" cy="30" r="10"/>
+        <circle cx="70" cy="30" r="10"/>
+      </svg>
+    `;
+      //el.className = 'fa fa-car';
+      // el.innerText = '🚗';
+      el.style.fontSize = '40px';
+      el.style.color = car.color;
+      el.style.textShadow = '0 0 3px #000';
+      el.style.backgroundSize = 'cover';
+      el.style.width = '50px';
+      el.style.height = '50px';
+      el.style.borderRadius = '50%';
+      el.style.cursor = 'pointer';
+      return new mapboxgl.Marker(el).setLngLat([car.lng, car.lat]);
+    }
+    return new mapboxgl.Marker();
+  }
 
-    this.cars.forEach(car => {
-      new mapboxgl.Marker({color: car.color})
-        .setLngLat([car.lng, car.lat])
-        .addTo(this.map!);
-    });
+  private addCarsToMap() {
+    const targetCar: Car = {lat: 3.3581, lng: 6.6144, color: this.getRandomColor()};
+    targetCar.marker = this.createCarMarker(targetCar).addTo(this.map!);
+    this.cars.push(targetCar);
+    for (let i = 0; i < 15; i++) {
+      const randomLat = 3.358 + ((Math.random() * 0.001) - 0.0005);
+      const randomLng = 6.614 + ((Math.random() * 0.001) - 0.0005);
+      const randomColor = this.getRandomColor();
+      const car: Car = {lat: randomLat, lng: randomLng, color: randomColor};
+      car.marker = this.createCarMarker(car).addTo(this.map!);
+      this.cars.push(car);
+    }
+  }
 
-    /**
-     * simulates intervals for change in positions of cars.
-     */
-
+  private simulateCarMovement() {
     setInterval(() => {
       this.cars.forEach(car => {
-        car.lat += Math.random() * 0.001;
-        car.lng += Math.random() * 0.001;
+        const latAdjustment = (Math.random() - 0.5) * 0.002;
+        const lngAdjustment = (Math.random() - 0.5) * 0.002;
+        car.lat = Math.min(Math.max(car.lat + latAdjustment, 3.3580), 3.3581);
+        car.lng = Math.min(Math.max(car.lng + lngAdjustment, 6.6143), 6.6145);
+
+        // Update the marker position
+        if (car.marker) {
+          car.marker.setLngLat([car.lng, car.lat]);
+        }
       });
     }, 1000);
   }
@@ -101,10 +138,10 @@ export class MapComponent implements OnInit {
   }
 
   getDrivers(): void {
-    const minLat = 0; // Set your minLat
-    const maxLat = 0; // Set your maxLat
-    const minLon = 0; // Set your minLon
-    const maxLon = 0; // Set your maxLon
+    const minLat = 3.3575;
+    const maxLat = 3.3585;
+    const minLon = 6.6142;
+    const maxLon = 6.6146;
 
     this.driverService.getDriversWithinBoundingBoxCoordinates(minLat, maxLat, minLon, maxLon)
       .then(drivers => {
@@ -114,4 +151,11 @@ export class MapComponent implements OnInit {
         console.error('Error: Close by Drivers could not be retrieved', error);
       });
   }
+}
+
+interface Car {
+  lat: number;
+  lng: number;
+  color: string;
+  marker?: mapboxgl.Marker; // Optional marker property
 }
