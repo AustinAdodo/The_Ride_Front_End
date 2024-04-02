a242183eb1f3c3271aaf7057d95bd4493cd2bbcb
Odia Adodo <subzbelow@gmail.com>
2024-04-02 14:05:39 +0100

    Updated

--- src/create-driver/create-driver.component.ts
+++ src/create-driver/create-driver.component.ts
@@ -2,24 +2,32 @@ import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
 import {environment} from '../environments/environment';
+import {NgIf} from "@angular/common";
+import {FormExtension} from "../Extensions/FormExtension";
 
 @Component({
   selector: 'app-create-driver',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './create-driver.component.html',
   styleUrls: ['./create-driver.component.css']
 })
 export class CreateDriverComponent {
   driver: string = "Driver";
+  hidePassword: boolean = true;
   driverForm = new FormGroup({
     firstName: new FormControl('', Validators.required),
     middleName: new FormControl('', Validators.required),
+    sex: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
     address: new FormControl('', Validators.required),
     carColor: new FormControl('', Validators.required),
     carBrand: new FormControl('', Validators.required),
-  });
+    Usertype: new FormControl('Driver'),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required)
+  }, {validators: FormExtension.confirmPasswordValidator('password', 'confirmPassword')});
 
   constructor(private http: HttpClient) {
   }
@@ -37,5 +45,9 @@ export class CreateDriverComponent {
         error: (error) => console.error('Error adding driver:', error)
       });
     }
+  }
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
   }
 }
