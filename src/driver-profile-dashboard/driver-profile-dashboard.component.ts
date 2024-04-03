640c61bb8ab785d760d99a35464219b6cafc8b79
Odia Adodo <subzbelow@gmail.com>
2024-04-03 21:16:13 +0100

    Update to component

--- src/driver-profile-dashboard/driver-profile-dashboard.component.ts
+++ src/driver-profile-dashboard/driver-profile-dashboard.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {WebSocketService} from "../services/WebSocketService";
 import {MatIcon} from "@angular/material/icon";
 import {Payload} from "../payload/Payload";
 import {CommonModule, CurrencyPipe} from "@angular/common";
 import {MatButton} from "@angular/material/button";
 import {MatTab, MatTabGroup, MatTabsModule} from "@angular/material/tabs";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {Subscription} from "rxjs";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-driver-profile-dashboard',
@@ -16,26 +17,44 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     MatButton,
     MatTabGroup,
     MatTab,
-    MatTabsModule,
+    MatTabsModule, ReactiveFormsModule,
   ],
   templateUrl: './driver-profile-dashboard.component.html',
   styleUrl: './driver-profile-dashboard.component.css'
 })
-export class DriverProfileDashboardComponent implements OnInit {
+
+export class DriverProfileDashboardComponent implements OnInit, OnDestroy {
   private webSocketService: WebSocketService;
+  private messagesSubscription!: Subscription;
+  public userMessage = new Payload();
+  protected shouldBeHidden: boolean = true;
 
   constructor(webSocketService: WebSocketService) {
     this.webSocketService = webSocketService;
+  }
+
+  ngOnInit(): void {
+    this.messagesSubscription = this.webSocketService.userUpdateMessages.subscribe(
+      (message: Payload) => {
+        this.userMessage = message;
+        console.log("Subscribed successfully via OnInit in DriverDashboardComponent")
+        console.log(message);
+        this.notification = {
+          type: message.type,
+          message: message.message
+        };
+      }
+    );
   }
 
   //get the current logged in driver
   driver = {
     firstname: 'Lawrence',
     username: 'odia@example.com',
-    rating: 4.5,
+    rating: 5.0,
     totalEarnings: 12000,
     tips: 1500,
-    totalTrips: 120,
+    totalTrips: 245,
     middleName: '',
     lastname: "Driver",
     carModel: 'Audi Rs7',
@@ -46,9 +65,10 @@ export class DriverProfileDashboardComponent implements OnInit {
     taxID: "NMI555789",
   };
 
+  //A new trip request from City Center to Green Park.
   notification = {
-    type: 'New trip',
-    message: 'A new trip request from City Center to Green Park.'
+    type: this.userMessage.type,
+    message: this.userMessage.message
   };
 
   recentTrips = [
@@ -62,17 +82,48 @@ export class DriverProfileDashboardComponent implements OnInit {
     {amount: 450, date: '2023-05-02'}
   ];
 
-  ngOnInit(): void {
-  }
+  DriverForm: FormGroup = new FormGroup({
+    firstName: new FormControl('Lawrence',),
+    sex: new FormControl('male'),
+    lastName: new FormControl('Good-Driver'),
+    rating: new FormControl(5.0),
+    photograph: new FormControl('/assets/Adodo_Austin.jpg')
+  });
 
   declineTrip() {
+    this.notification.type = "None";
+    this.notification.message="No new message";
+    //driverService.Find_Next_Driver
   }
 
   acceptTrip() {
-    const driverPayload = new Payload();
-    driverPayload.sex = 'male';
-    // Send the acceptance message
+    const name = `${this.DriverForm.get('firstName')?.value} ${this.DriverForm.get('lastName')?.value}`;
+    const sex = this.DriverForm.get('sex')?.value ?? "";
+    const number = '09384721';
+    const driverPayload: Payload = new Payload({
+      sex: sex,
+      message: `Driver is on ${sex === 'male' ? 'his' : 'her'} way. Call on ${number}`,
+      name: name,
+      phoneNumber:number,
+      rating: this.DriverForm.get('rating')?.value ?? 0.0,
+      photoUrl: this.DriverForm.get('photograph')?.value ?? '',
+      type: 'None',
+    });
+    this.notification.type = "None";
+    this.notification.message ="No Message";
     this.webSocketService.sendDriverAcceptance(driverPayload);
   }
 
+  getRatingArray() {
+    return Array(Math.round(this.driver.rating)).fill(0).map((x, i) => i);
+  }
+
+ /**
+ * Lifecycle to destroy listeners and free up memory
+  */
+  ngOnDestroy() {
+    if (this.messagesSubscription) {
+      this.messagesSubscription.unsubscribe();
+    }
+  }
 }
