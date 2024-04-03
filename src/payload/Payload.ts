e00cea89375dbf70cab2a2f93cdde4f6fdd379b6
Odia Adodo <subzbelow@gmail.com>
2024-04-03 13:03:14 +0100

    Updated

--- src/payload/Payload.ts
+++ src/payload/Payload.ts
@@ -1,12 +1,18 @@
+/**
+ * payload request types (General Message, New trip, None)
+ */
 export class Payload {
   name: string = "Driver";
   phoneNumber: string = "xxx";
   rating: number = 5.0;
   hasAccepted: boolean = true;
-  message: string = "Your Driver is on his way";
-  photoUrl: string = '';
-  sex: string = '';
-  type:string='General Message';
+  message: string = "No Message";
+  photoUrl: string = "";
+  sex: string = "";
+  type: string = 'General Message';
+  topic: string = '';
+  status: string = 'Front-End';
+  location: string = '';
 
   constructor(init?: Partial<Payload>) {
     Object.assign(this, init);
