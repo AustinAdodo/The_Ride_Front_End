890b00f97ccf7887a23bcd74f01c42f541ee8cdc
Odia Adodo <subzbelow@gmail.com>
2024-04-03 21:16:45 +0100

    Update to component

--- src/user-profile-dashboard/user-profile-dashboard.component.ts
+++ src/user-profile-dashboard/user-profile-dashboard.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from "@angular/common";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatDialog, MatDialogActions, MatDialogContent} from '@angular/material/dialog';
+import {DriverModalComponent} from '../driver-modal/driver-modal.component';
+import {WebSocketService} from "../services/WebSocketService";
+import {Payload} from "../payload/Payload";
+import {Subscription} from "rxjs";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton, MatIconButton} from "@angular/material/button";
 
 interface RecentTrip {
   destination: string;
@@ -19,42 +26,104 @@ interface FavoritePlace {
   imports: [
     FormsModule,
     NgOptimizedImage,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatIcon,
+    MatIconButton,
+    MatDialogContent,
+    MatDialogActions,
+    MatButton
   ],
   templateUrl: './user-profile-dashboard.component.html',
   styleUrls: ['./user-profile-dashboard.component.css']
 })
-export class UserProfileDashboardComponent implements OnInit {
+
+export class UserProfileDashboardComponent implements OnInit, OnDestroy {
+  userHasNotBooked: boolean = true;
+  private messagesSubscription!: Subscription;
   recentTrips: RecentTrip[] = [];
   favoritePlaces: FavoritePlace[] = [];
   showForm: boolean = false;
-  Rating : string = '/assets/5stars.png';
-  mastercard :string= '/assets/mastercard.png';
+  mastercard: string = '/assets/mastercard.png';
+  shouldBeHidden: boolean = true;
+  // private userdata: Payload = new Payload({sex:'male',rating:5.0,name:'Adodo Austin'});
 
-  constructor() { }
+  UserForm = new FormGroup({
+    firstName: new FormControl('Austin',),
+    Location: new FormControl('Ikeja City Mall'),
+    Destination: new FormControl('', Validators.required),
+    sex: new FormControl('male'),
+    lastName: new FormControl('Adodo'),
+    rating: new FormControl(5.0),
+    photograph: new FormControl('/assets/Adodo_Austin.jpg')
+  });
+
+  constructor(private webSocketService: WebSocketService, public dialog: MatDialog) {
+  }
 
   ngOnInit(): void {
     this.loadRecentTrips();
     this.loadFavoritePlaces();
+    this.messagesSubscription = this.webSocketService.driverUpdateMessages.subscribe(
+      (message: Payload) => {
+        //each message updates from driver are shown in real time.
+        console.log('Message received:', message);
+        console.log('Attempting to open modal');
+        this.showDriverModal(message);
+      }
+    );
   }
 
   loadRecentTrips(): void {
     this.recentTrips = [
-      { destination: 'Park-Lane', date: '2024-03-22', status: 'Completed' },
-      { destination: 'Country Club', date: '2024-02-15', status: 'Completed' }
+      {destination: 'Park-Lane', date: '2024-03-22', status: 'Completed'},
+      {destination: 'Country Club', date: '2024-02-15', status: 'Completed'}
     ];
   }
 
   loadFavoritePlaces(): void {
     this.favoritePlaces = [
-      { name: 'Ikeja City Mall', location: 'Lagos' },
-      { name: 'Black bell Restaurant', location: 'Lagos' }
+      {name: 'Ikeja City Mall', location: 'Lagos'},
+      {name: 'Black bell Restaurant', location: 'Lagos'}
       // Add more places here
     ];
   }
-  onSubmit() {
-    // Handle form submission here
-    console.log('Form submitted');
-    this.showForm = false;
+
+  showDriverModal(driverData: Payload) {
+    this.dialog.open(DriverModalComponent, {
+      width: '250px',
+      data: driverData,
+      position: {top: '10px'},
+      disableClose: true
+    });
   }
+
+  sendCustomerTripRequest() {
+    const name = `${this.UserForm.get('firstName')?.value} ${this.UserForm.get('lastName')?.value}`;
+    const address = this.UserForm.get('Location')?.value ?? ''
+    const currentUserPayload: Payload = new Payload({
+      sex: this.UserForm.get('sex')?.value ?? "",
+      message: `You have a new request from ${name}, currently at ${address}.
+        Hey!, I Would be glad if you could pick me up.`,
+      name: name,
+      phoneNumber: '09384721',
+      rating: this.UserForm.get('rating')?.value ?? 0.0,
+      photoUrl: this.UserForm.get('photograph')?.value ?? '',
+      type: 'New trip',
+      location: address
+    });
+    this.showForm = !this.showForm;
+    return this.webSocketService.sendCustomerTripRequest(currentUserPayload);
+  }
+
+  getRatingArray() {
+    return Array(Math.round(5.0)).fill(0).map((x, i) => i);
+  }
+
+  ngOnDestroy() {
+    if (this.messagesSubscription) {
+      this.messagesSubscription.unsubscribe();
+    }
+  }
+
 }
