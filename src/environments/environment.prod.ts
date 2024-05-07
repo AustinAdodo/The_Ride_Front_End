5990bdf7e85b7cba7f4e62271556b34de65cfc3d
Odia Adodo <subzbelow@gmail.com>
2024-05-07 08:48:06 +0100

    Update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
+  mapBoxGlAccessToken: process.env['MAPBOX_GL_ACCESS_TOKEN'] as string,
+  baseUri:"http://localhost:8080",
+  webSocketUrl:'http://localhost:8080/ws'
+};
