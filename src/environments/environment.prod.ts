7b09c6bc4e0c4fbc1ff70045129d7efcfadde980
Odia Adodo <subzbelow@gmail.com>
2024-05-07 09:02:11 +0100

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
+
 export const environment = {
   production: true,
   googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
   mapBoxGlAccessToken: process.env['MAPBOX_GL_ACCESS_TOKEN'] as string,
-  baseUri:"http://localhost:8080",
-  webSocketUrl:'http://localhost:8080/ws'
+  baseUri:"https://theridebackend.us.aldryn.io:8080",
+  webSocketUrl:'https://theridebackend.us.aldryn.io:8080/ws'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@ export const environment = {
   production: true,
   googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
   mapBoxGlAccessToken: process.env['MAPBOX_GL_ACCESS_TOKEN'] as string,
-  baseUri:"http://localhost:8080",
-  webSocketUrl:'http://localhost:8080/ws'
+  // baseUri:"http://localhost:8080",
+  // webSocketUrl:'http://localhost:8080/ws'
 };
 
 
