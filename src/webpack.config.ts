99ee386db0172b35dd0b672fbeb6bc75adbfb85b
Odia Adodo <subzbelow@gmail.com>
2024-05-27 09:58:26 +0100

    Update

--- src/webpack.config.ts
+++ src/webpack.config.ts
@@ -2,7 +2,8 @@ import * as webpack from 'webpack';
 
 /**
  * NB: Note custom webpack configuration must be installed to allow custom webpack in angular.json
- * use npm install @angular-builders/custom-webpack
+ * use npm install @angular-builders/custom-webpack @angular-devkit/build-angular
+ * <p>
  * Also remember to change @angular-devkit/build-angular:application to @angular-builders/custom-webpack as the latter
  * supports configuration for custom webpack.
  */
